d42c6fbdeda500205098b7e2b312e69a1ed2b510
Ulises Hedman <ulyhedman@hotmail.com>
2023-09-29 09:25:28 -0300

    wip(cek)

--- src/CEK.hs
+++ src/CEK.hs
@@ -11,25 +11,40 @@ Stability   : experimental
 module CEK where
 
 import Lang
-import MonadFD4 (MonadFD4)
+import MonadFD4 ( MonadFD4, failFD4 )
+import Common ( Pos( NoPos ) )
+import Subst ( close, close2 )
 
 data Val = 
-    Var Name
-  | Fun Name Val
-  | Fix Name Name Val
+    Nat Const
+  | Clos Clos
 
-Type Env = [(Name, Val)]
+data Clos = 
+    ClFun Env Name TTerm
+  | ClFix Env Name Name TTerm  
 
-data Frame = 
-    FrAppL Frame TTerm
-  | FrAppR Val Frame
-  | FrIfz Frame TTerm TTerm
-  | FrBOpL Frame TTerm
-  | FrBOpR Val Frame
-  | FrPrint String Frame
+type Env = [(Name, Val)]
 
-Type Kont = [Frame]
+data Frame =
+    FrApp Env TTerm
+  | FrClos Clos
+  | FrIfz Env TTerm TTerm
+  | FrBOpL Env TTerm
+  | FrBOpR TTerm
+  | FrPrint String
+  | FrLet Env TTerm
+
+type Kont = [Frame]
 
 seek :: MonadFD4 m => TTerm -> Env -> Kont -> m Val
+seek = failFD4 "Not implemented"
 
 destroy :: MonadFD4 m => Val -> Kont -> m Val
+destroy = failFD4 "Not implemented"
+
+cek :: MonadFD4 m => TTerm -> m TTerm
+cek t = do t' <- seek t [] []
+           case t' of
+             Nat c -> return $ Const (NoPos, NatTy) c
+             Clos (ClFun e n s) -> return $ Lam (NoPos, NatTy) n NatTy (close n s) -- Wip
+             Clos (ClFix e f n s) -> return $ Fix (NoPos, NatTy) f NatTy n NatTy (close2 f n s) -- Wip
