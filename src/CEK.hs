05ae8025229e16037151cb5f9e5ebcc8998b7f0f
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2023-10-03 15:27:40 -0300

    change(Val with info and types)

--- src/CEK.hs
+++ src/CEK.hs
@@ -12,16 +12,17 @@ module CEK where
 
 import Lang
 import MonadFD4 ( MonadFD4, printFD4, failFD4, lookupDecl )
-import Common ( Pos( NoPos ) )
+import Common ( Pos )
 import Subst ( close, close2, open, open2)
+import PPrint ( pp )
 
 data Val = 
-    Nat Int
+    Nat (Pos, Ty) Int
   | Clos Clos
 
 data Clos = 
-    ClFun Env Name TTerm
-  | ClFix Env Name Name TTerm
+    ClFun (Pos, Ty) Env Name Ty TTerm
+  | ClFix (Pos, Ty) Env Name Ty Name Ty TTerm
 
 type Env = [Val]
 
@@ -56,37 +57,45 @@ seek (V _ (Global n)) env k =
      case res of
        Nothing -> failFD4 "Variable not found"
        Just v -> seek v env k
-seek (Const _ (CNat n)) env k = destroy (Nat n) k
-seek (Lam _ x _ t) env k = 
-  destroy (Clos (ClFun env x (open x t))) k
-seek (Fix _ f _ x _ t) env k = 
-  destroy (Clos (ClFix env f x (open2 f x t))) k
+seek (Const i (CNat n)) env k = destroy (Nat i n) k
+seek (Lam i x xty t) env k = 
+  destroy (Clos (ClFun i env x xty (open x t))) k
+seek (Fix i f fty x xty t) env k = 
+  destroy (Clos (ClFix i env f fty x xty (open2 f x t))) k
 seek (Let _ x _ s t) env k = 
   seek s env (FrLet env x (open x t):k)
 
+val2tterm :: MonadFD4 m => Val -> m TTerm
+val2tterm (Nat i n) = return $ Const i (CNat n)
+val2tterm (Clos (ClFun i env x xty t)) = 
+  do 
+     return $ Lam i x xty (close x t) -- sust en t
+val2tterm (Clos (ClFix i env f fty x xty t)) = 
+  do 
+     return $ Fix i f fty x xty (close2 f x t) -- sust en t
+
 evalOp :: BinaryOp -> Val -> Val -> Val
-evalOp Add (Nat n) (Nat n')= Nat (n+n')
-evalOp Sub (Nat n) (Nat n')= Nat (n-n')
+evalOp Add (Nat i n) (Nat i' n') = Nat i (n+n')
+evalOp Sub (Nat i n) (Nat i' n')= Nat i (n-n')
 evalOp _ _ _ = error "Binary operation with closures"
 
 destroy :: MonadFD4 m => Val -> Kont -> m Val
 destroy v (FrPrint s:k) = 
-  do printFD4 s -- agregar str(v)
+  do vt <- val2tterm v
+     pv <- pp vt
+     printFD4 $ s ++ pv
      destroy v k
-destroy (Nat n) (FrBOpL env op u:k) = seek u env (FrBOpR (Nat n) op:k)
-destroy (Nat n') (FrBOpR (Nat n) op:k) = destroy (evalOp op (Nat n) (Nat n')) k
-destroy (Nat 0) (FrIfz env t e:k) = seek t env k
-destroy (Nat np) (FrIfz env t e:k) = seek e env k
+destroy (Nat i n) (FrBOpL env op u:k) = seek u env (FrBOpR (Nat i n) op:k)
+destroy (Nat i' n') (FrBOpR (Nat i n) op:k) = destroy (evalOp op (Nat i n) (Nat i' n')) k
+destroy (Nat i 0) (FrIfz env t e:k) = seek t env k
+destroy (Nat i np) (FrIfz env t e:k) = seek e env k
 destroy (Clos c) (FrApp env t:k) = seek t env (FrClos c:k)
-destroy v (FrClos (ClFun env x t):k) = seek t (v:env) k
-destroy v (FrClos (ClFix env f x t):k) = seek t (Clos (ClFix env f x t):v:env) k
+destroy v (FrClos (ClFun i env x xty t):k) = seek t (v:env) k
+destroy v (FrClos (ClFix i env f fty x xty t):k) = seek t (Clos (ClFix i env f fty x xty t):v:env) k
 destroy v (FrLet env x t:k) = seek t (v:env) k
 destroy v [] = return v
 destroy _ _ = failFD4 "Bad args in destroy"
 
-cek :: MonadFD4 m => TTerm -> m TTerm
-cek t = do t' <- seek t [] []
-           case t' of
-             Nat c -> return $ Const (NoPos, NatTy) (CNat c)
-             Clos (ClFun e n s) -> return $ Lam (NoPos, NatTy) n NatTy (close n s) -- Wip
-             Clos (ClFix e f n s) -> return $ Fix (NoPos, NatTy) f NatTy n NatTy (close2 f n s) -- Wip
+evalCEK :: MonadFD4 m => TTerm -> m TTerm
+evalCEK t = do t' <- seek t [] []
+               val2tterm t'
