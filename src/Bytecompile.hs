55e3637287ccd2769f88c16a2831c4f98e8086dd
Ulises Hedman <ulyhedman@hotmail.com>
2023-10-05 19:03:07 -0300

    print tests passed

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-|
 Module      : Bytecompile
 Description : Compila a bytecode. Ejecuta bytecode.
@@ -17,7 +18,6 @@ module Bytecompile
 
 import Lang
 import MonadFD4
-import Subst ( open, close, open2 )
 
 import qualified Data.ByteString.Lazy as BS
 import Data.Binary ( Word32, Binary(put, get), decode, encode )
@@ -108,8 +108,8 @@ bcc (V i (Bound n)) = return [ACCESS,n]
 bcc (V i (Free nm)) = failFD4 "Las variables libres deberian transformarse a indices de de Bruijn"
 bcc (V i (Global nm)) = failFD4 "Las variables globales deberian transformarse a indices de de Bruijn"
 bcc (Const i (CNat n)) = return [CONST,n]
-bcc (Lam i n t s) = 
-  do s' <- bcc (open n s)
+bcc (Lam i n t (Sc1 s)) = 
+  do s' <- bcc s
      return $ [FUNCTION, length s']++s'++[RETURN]
 bcc (App i l r) = 
   do l' <- bcc l
@@ -117,7 +117,7 @@ bcc (App i l r) =
      return $ l'++r'++[CALL]
 bcc (Print i str t) = 
   do t' <- bcc t
-     return $ [PRINT]++string2bc str++[NULL]++[PRINTN]++t'
+     return $ t'++[PRINT]++string2bc str++[NULL]
 bcc (BinaryOp i Add l r) = 
   do l' <- bcc l
      r' <- bcc r
@@ -126,8 +126,8 @@ bcc (BinaryOp i Sub l r) =
   do l' <- bcc l
      r' <- bcc r
      return $ l'++r'++[SUB]
-bcc (Fix i f fty x xty s) = 
-  do s' <- bcc (open2 f x s)
+bcc (Fix i f fty x xty (Sc2 s)) = 
+  do s' <- bcc s
      return $ [FUNCTION, length s'] ++ s' ++ [RETURN, FIX]
 bcc (IfZ i c t e) = -- usar JUMP?
   do c' <- bcc c
@@ -136,10 +136,10 @@ bcc (IfZ i c t e) = -- usar JUMP?
      case c' of
        [0] -> return t'
        [n] -> return e'
-       _ -> failFD4 "Resultado inesperado"
-bcc (Let i x xty e1 e2) = 
+       res -> failFD4 "Resultado inesperado" 
+bcc (Let i x xty e1 (Sc1 e2)) = 
   do e1' <- bcc e1
-     e2' <- bcc (open x e2)
+     e2' <- bcc e2
      return $ e1'++[SHIFT]++e2'++[DROP]
 
 -- ord/chr devuelven los codepoints unicode, o en otras palabras
@@ -153,7 +153,7 @@ bc2string = map chr
 translate :: [Decl TTerm] -> TTerm
 translate [] = error "Lista de declaraciones vacia"
 translate [Decl p n b] = b
-translate (Decl p n b:ds) = Let (p, getTy b) n (getTy b) b (close n (translate ds))
+translate (Decl p n b:ds) = Let (p, getTy b) n (getTy b) b (Sc1 (translate ds))
 translate _ = error "No se esperaba una declaracion de tipo"
 
 bytecompileModule :: MonadFD4 m => Module -> m Bytecode
@@ -191,10 +191,12 @@ runBC bc = go (bc, [], [])
                                      in go (c, e, C (efix, cf):s)
         go (SHIFT:c, e, v:s) = go (c, v:e, s)
         go (DROP:c, _:e, s) = go (c, e, s)
-        go (PRINTN:c, e, N n:s) = do liftIO $ print (show n)
-                                     go (c, e, N n:s)
-        go (PRINT:c, e, N n:s) = do liftIO $ print (show n)
-                                    go (c, e, N n:s)
+        -- go (PRINTN:c, e, N n:s) = do printFD4 (show n)
+        --                              go (c, e, N n:s)
+        go (PRINT:c, e, N n:s) = do let str = takeWhile (/= NULL) c
+                                    let c' = tail $ dropWhile (/= NULL) c
+                                    printFD4 $ bc2string str ++ show n
+                                    go (c', e, s)
         go (JUMP:n:c, e, s) = go (drop n c, e, s)
         go (STOP:_, _, _) = return ()
-        go (_, _, _) = error "Patron no reconocido"
+        go (c, _, _) = error $ "Patron no reconocido" ++ show c
