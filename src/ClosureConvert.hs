e7c3ed40cd015a8fdab27b7282622e6f0d63e674
Ulises Hedman <ulyhedman@hotmail.com>
2023-11-14 17:20:13 -0300

    wip(fun args)

--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -8,7 +8,7 @@ import Subst ( open ) --, open2 )
 import MonadFD4 (MonadFD4)
 
 -- Closure convert y hoisting
-convert :: TTerm -> [(Name, Ir)] -> StateT Int (Writer [IrDecl]) Ir
+convert :: TTerm -> [(Name, Ir)] -> StateT (Name, Int) (Writer [IrDecl]) Ir
 convert (V _ Bound {}) _ = error "No se esperaban variables ligadas" 
 convert (V _ (Free n)) list = case lookup n list of
                                 Just a -> return a
@@ -16,7 +16,7 @@ convert (V _ (Free n)) list = case lookup n list of
 convert (V _ (Global n)) _ = return $ IrGlobal n
 convert (Const _ c) _ = return $ IrConst c
 convert (Lam (_,ty) x xty body) list = 
-  do funName <- getFreshName
+  do funName <- getFunName
      cloName <- getFreshName
      let ir = IrAccess (IrVar cloName) (ty2ir xty) 0
      closureBody <- convert (open x body) ((x,ir):list)
@@ -25,7 +25,7 @@ convert (Lam (_,ty) x xty body) list =
 convert (App (_,ty) l r) list = 
   do funcIr <- convert l list
      argIr <- convert r list
-     return (IrCall funcIr [argIr] (ty2ir ty))
+     return (IrCall funcIr [funcIr, argIr] (ty2ir ty))
 convert (Print _ s t) list = 
   do t' <- convert t list
      return $ IrPrint s t' 
@@ -44,12 +44,14 @@ convert (IfZ _ c t e) list =
      e' <- convert e list
      return $ IrIfZ c' t' e'
 convert (Let _ x ty def body) list = 
-  do def' <- convert def list 
+  do setFunName x
+     def' <- convert def list 
      body' <- convert (open x body) ((x, def'):list)
      return $ IrLet x (ty2ir ty) def' body'
 
-convertDecl :: Decl TTerm -> StateT Int (Writer [IrDecl]) Ir
-convertDecl (Decl _ x body) = do b <- convert body []
+convertDecl :: Decl TTerm -> StateT (Name, Int) (Writer [IrDecl]) Ir
+convertDecl (Decl _ x body) = do setFunName x
+                                 b <- convert body []
                                  case b of
                                    MkClosure {} -> return ()
                                    _ -> do freshName <- getFreshName
@@ -58,15 +60,22 @@ convertDecl (Decl _ x body) = do b <- convert body []
 convertDecl DeclTy {} = error "No se soportan sinonimos de tipo" 
 
 runCC :: MonadFD4 m => [Decl TTerm] -> m [IrDecl]
-runCC decls = do let ird = snd $ runWriter (runStateT (mapM convertDecl decls) 0)
+runCC decls = do let ird = snd $ runWriter (runStateT (mapM convertDecl decls) ("", 0))
                 --  printFD4 $ show ird
                  return ird
 
 -- Funciones auxiliares
-getFreshName :: StateT Int (Writer [IrDecl]) [Char]
-getFreshName = do n <- get
-                  modify (+1)
-                  return $ show n
+getFreshName :: StateT (Name, Int) (Writer [IrDecl]) Name
+getFreshName = do (name, int) <- get
+                  put (name, int+1)
+                  return $ show int
+
+getFunName :: StateT (Name, Int) (Writer [IrDecl]) Name
+getFunName = do (name, int) <- get
+                return name
+
+setFunName :: Name -> StateT (Name, Int) (Writer [IrDecl]) ()
+setFunName newName = do modify (\(_, int) -> (newName, int))
 
 ty2ir :: Ty -> IrTy
 ty2ir NatTy = IrInt
